2816b1548b52964488d4a086956779bebd7002be
Dan DeMeyere <dan@thredup.com>
2013-07-20 17:44:17 -0700

    4.0 boilerplate commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Website::Application.config.secret_key_base = 'c2fc071dac423a6d314eafc98a720f08af58306a31d64d675c772bacca378008e66aec95cb1c564f766dd7815a20628bba729bb0e76c7115399b563e30bbd066'
