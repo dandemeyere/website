bacea8c5c40519e64619ccb9ee41ea45102acda0
Dan DeMeyere <dan@thredup.com>
2013-12-11 09:03:34 -0800

    [Bug] Forgot to exclude drafts in my named scope.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,5 +1,5 @@
 class BlogPost < ActiveRecord::Base
-  scope :published, lambda { where("published_at < ?", Time.now) }
+  scope :published, lambda { where("published_at < ?", Time.now).where(draft: false) }
   scope :next_post, lambda { |post| published.where('published_at < ?', post.published_at).order("published_at DESC") }
   scope :previous_post, lambda { |post| published.where('published_at > ?', post.published_at).order("published_at ASC") }
 end
