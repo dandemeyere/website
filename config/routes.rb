90696525625fea29aa3438429dbe53a967997c13
Dan DeMeyere <dan@thredup.com>
2014-04-04 10:50:04 -0700

    [Bug] Fixing some old URL mapping from indexed URLs from the Squarespace days

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Website::Application.routes.draw do
   resources :messages
   get "/rails-engineer" => redirect("/blog/service-oriented-rails-engineer")
+  # Old routes mapping
+  get "/blog/2010/8/31/website-of-the-week-thredup" => redirect("/blog/website-of-the-week-thredup-com/")
+
+  get '/blog/:year/:month/:day/:permalink' => "blog_posts#show"
   get "/blog", to: "blog_posts#index"
   get "/blog/rss", to: "blog_posts#rss"
   get "/blog/:permalink", to: "blog_posts#show"
